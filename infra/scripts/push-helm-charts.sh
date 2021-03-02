7d4cfad185ead621c438e37725e167b8b5518de0
Willem Pienaar <git@willem.co>
2021-03-02 11:10:31 -0800

    Use pinned helm-gcs version

--- infra/scripts/push-helm-charts.sh
+++ infra/scripts/push-helm-charts.sh
@@ -10,10 +10,10 @@ fi
 bucket=gs://feast-helm-charts
 repo_url=https://feast-helm-charts.storage.googleapis.com/
 
-helm plugin install https://github.com/hayorov/helm-gcs.git || true
+helm plugin install https://github.com/hayorov/helm-gcs.git --version 0.2.2  || true
 
 helm repo add feast-helm-chart-repo $bucket
 
 helm package infra/charts/feast-spark
 
-helm gcs push feast-spark-${1}.tgz feast-helm-chart-repo
+helm gcs push --force feast-spark-${1}.tgz feast-helm-chart-repo
