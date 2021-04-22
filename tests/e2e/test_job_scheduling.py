6c137a5e6e198b50d25299e668b8350d70f4b04b
Khor Shu Heng <khor.heng@gojek.com>
2021-04-22 15:13:24 +0800

    Fix linting
    Signed-off-by: Khor Shu Heng <khor.heng@gojek.com>

--- tests/e2e/test_job_scheduling.py
+++ tests/e2e/test_job_scheduling.py
@@ -1,13 +1,15 @@
+import uuid
+
 import pytest as pytest
 
-from feast import Entity, Feature, FeatureTable, FileSource, ValueType
+from feast import Client, Entity, Feature, FeatureTable, FileSource, ValueType
 from feast.data_format import ParquetFormat
 from feast_spark import Client as SparkClient
 
 
 @pytest.mark.env("k8s")
 def test_schedule_batch_ingestion_jobs(
-    pytestconfig, feast_spark_client: SparkClient
+    pytestconfig, feast_client: Client, feast_spark_client: SparkClient
 ):
     entity = Entity(name="s2id", description="S2id", value_type=ValueType.INT64,)
     batch_source = FileSource(
@@ -18,11 +20,17 @@ def test_schedule_batch_ingestion_jobs(
         date_partition_column="datetime",
     )
     feature_table = FeatureTable(
-        name="drivers",
+        name=f"schedule_{str(uuid.uuid4())}",
         entities=["s2id"],
         features=[Feature("unique_drivers", ValueType.INT64)],
         batch_source=batch_source,
     )
+    feast_client.apply(entity)
+    feast_client.apply(feature_table)
 
-    feast_spark_client.schedule_offline_to_online_ingestion(feature_table, 1, "0 0 * * *")
-    feast_spark_client.unschedule_offline_to_online_ingestion(feature_table, feast_spark_client._feast.project)
+    feast_spark_client.schedule_offline_to_online_ingestion(
+        feature_table, 1, "0 0 * * *"
+    )
+    feast_spark_client.unschedule_offline_to_online_ingestion(
+        feature_table, feast_spark_client._feast.project
+    )
