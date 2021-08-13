848c74b7696c736500428e087cefffc286a59e03
Oleksii Moskalenko <moskalenko.alexey@gmail.com>
2021-08-13 13:38:22 +0800

    Pandas is missing in spark job base image (#88)
    add pandas to spark base image
    
    Signed-off-by: Oleksii Moskalenko <moskalenko.alexey@gmail.com>
    
    upper bound for numpy version
    
    Signed-off-by: Oleksii Moskalenko <moskalenko.alexey@gmail.com>
    
    fix
    
    Signed-off-by: Oleksii Moskalenko <moskalenko.alexey@gmail.com>

--- spark/ingestion/src/test/resources/python/setup.sh
+++ spark/ingestion/src/test/resources/python/setup.sh
@@ -8,11 +8,11 @@ if [[ -f "$DESTINATION/libs.tar.gz" ]]; then
     echo "$DESTINATION/libs.tar.gz exists."
 else
   tmp_dir=$(mktemp -d)
-  pip3 install -t ${tmp_dir}/libs great-expectations pyarrow==2.0.0
+  pip3 install -t ${tmp_dir}/libs great-expectations==0.13.2 pyarrow==2.0.0
   cd $tmp_dir && tar -czf libs.tar.gz libs/ && mv libs.tar.gz $DESTINATION/libs.tar.gz
 fi
 
 # 2. Pickle python udf
 cd $CURRENT_PATH
-pip3 install great-expectations setuptools pyspark==3.0.1
+pip3 install great-expectations==0.13.2 setuptools pyspark==3.0.1
 python3 udf.py $DESTINATION/udf.pickle
