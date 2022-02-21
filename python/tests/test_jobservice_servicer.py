363f240e54c275fb38b28c9275dc682523701e13
Keshav <keshav2211@gmail.com>
2022-02-21 11:16:47 +0530

    add config setting for whitelisting feast job types (#110)
    * add config setting for whitelisting feast job types
    
    * add config setting for whitelisting feast job types
    
    Signed-off-by: KeshavSharma <keshav.sharma@gojek.com>
    
    Co-authored-by: KeshavSharma <keshav.sharma@gojek.com>

--- python/tests/test_jobservice_servicer.py
+++ python/tests/test_jobservice_servicer.py
@@ -21,3 +21,19 @@ def test_feature_table_whitelist():
         job_servicer = JobServiceServicer(job_client)
         assert not job_servicer.is_feature_table_whitelisted("project2", "table1")
         assert job_servicer.is_feature_table_whitelisted("project1", "table1")
+
+
+def test_job_type_default_whitelist():
+    feast_client = Client()
+    job_client = JobClient(feast_client)
+    job_servicer = JobServiceServicer(job_client)
+    assert job_servicer.is_job_type_whitelisted("STREAM_INGESTION")
+
+
+def test_job_type_whitelist():
+    feast_client = Client(whitelisted_job_types="STREAM_INGESTION,BATCH_INGESTION")
+    job_client = JobClient(feast_client)
+    job_servicer = JobServiceServicer(job_client)
+    assert job_servicer.is_job_type_whitelisted("STREAM_INGESTION")
+    assert job_servicer.is_job_type_whitelisted("BATCH_INGESTION")
+    assert not job_servicer.is_job_type_whitelisted("HISTORICAL_RETRIEVAL")
