77d86f23cca25fa3f498091915daa4d609ec1fcf
Khor Shu Heng <khor.heng@gojek.com>
2022-02-23 15:34:06 +0800

    update version to 0.2.20
    Signed-off-by: Khor Shu Heng <khor.heng@gojek.com>

--- spark/ingestion/src/main/scala/feast/ingestion/IngestionJob.scala
+++ spark/ingestion/src/main/scala/feast/ingestion/IngestionJob.scala
@@ -34,7 +34,7 @@ object IngestionJob {
 
   val parser = new scopt.OptionParser[IngestionJobConfig]("IngestionJob") {
     // ToDo: read version from Manifest
-    head("feastSpark.ingestion.IngestionJob", "0.2.19")
+    head("feastSpark.ingestion.IngestionJob", "0.2.20")
 
     opt[Modes]("mode")
       .action((x, c) => c.copy(mode = x))
