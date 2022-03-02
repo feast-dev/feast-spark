4fcf740c0d62337ee00b15fbe8c586b059e77d7e
Terence Lim <terencelimxp@gmail.com>
2022-03-02 14:33:02 +0800

    Add last processed event timestamp to streaming job (#120)
    * Add last processed event timestamp
    
    Signed-off-by: Terence Lim <terencelimxp@gmail.com>
    
    * Fix dependency
    
    Signed-off-by: Terence Lim <terencelimxp@gmail.com>

--- spark/ingestion/src/main/scala/org/apache/spark/metrics/source/StreamingMetricSource.scala
+++ spark/ingestion/src/main/scala/org/apache/spark/metrics/source/StreamingMetricSource.scala
@@ -19,6 +19,8 @@ package org.apache.spark.metrics.source
 import org.apache.spark.metrics.AtomicLongGauge
 import org.apache.spark.sql.streaming.StreamingQueryProgress
 
+import java.time.Instant
+
 class StreamingMetricSource extends BaseMetricSource {
   override val sourceName: String = StreamingMetricSource.sourceName
 
@@ -30,11 +32,19 @@ class StreamingMetricSource extends BaseMetricSource {
     metricRegistry.register(gaugeWithLabels("processed_rows_per_second"), new AtomicLongGauge())
   private val LAST_CONSUMED_KAFKA_TIMESTAMP_GAUGE =
     metricRegistry.register(gaugeWithLabels("last_consumed_kafka_timestamp"), new AtomicLongGauge())
+  private val LAST_PROCESSED_EVENT_TIMESTAMP_GAUGE =
+    metricRegistry.register(
+      gaugeWithLabels("last_processed_event_timestamp"),
+      new AtomicLongGauge()
+    )
 
   def updateStreamingProgress(progress: StreamingQueryProgress): Unit = {
     BATCH_DURATION_GAUGE.value.set(progress.batchDuration)
     INPUT_ROWS_PER_SECOND_GAUGE.value.set(progress.inputRowsPerSecond.toLong)
     PROCESSED_ROWS_PER_SECOND_GAUGE.value.set(progress.processedRowsPerSecond.toLong)
+
+    val epochTimestamp = Instant.parse(progress.timestamp).getEpochSecond
+    LAST_PROCESSED_EVENT_TIMESTAMP_GAUGE.value.set(epochTimestamp)
   }
 
   def updateKafkaTimestamp(timestamp: Long): Unit = {
